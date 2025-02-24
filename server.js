2a336b7a92a8d95ee65b163a851f5ec51711d441
youssefDevp <youssefmed212@gmail.com>
2025-02-24 20:26:44 +0100

    first upload

---
+++ server.js
@@ -0,0 +1,96 @@
+
+import { createServer } from 'node:http';
+import fs from 'node:fs';
+import querystring from 'querystring';
+import * as path from 'path';
+
+
+
+
+
+
+
+
+
+const server = createServer((req, res) => {
+
+var url = req.url;
+var fileName = "";
+
+if (url === "/") {
+    fileName = "index.html";
+}
+
+else if (url === "/thanks") {
+    fileName = "thanksPage.html";
+} 
+
+else if (url === "/formulario")  {
+    fileName = "formulario.html";
+}
+    
+
+   
+
+if (req.method=='POST' && req.url == '/formulario') {
+    let body =""
+    req.on('data', chunk => {
+
+        body += chunk.toString(); // Collect chunks
+    });
+
+    req.on('end', () => {
+        console.log('Full Request body:', body);
+        const formData = querystring.parse(body);
+        console.log('form data: ', formData)
+
+        console.log('form data: ', formData.puesto)
+        res.end();
+    
+        const nombre = formData.nombre;
+        const apellido = formData.apellido;
+        const email = formData.email;
+        const phone = formData.phone;
+        const experiencia = formData.experiencia;
+        const puesto = formData.puesto;
+
+        const data = `Name: ${nombre}, LastName : ${apellido}, Email: ${email},Phone: ${phone}, experiencia: ${experiencia}, Puesto: ${puesto} \n`;
+
+        fs.appendFile(path.join('', 'data.txt'), data, (err) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error saving data');
+                return;
+            }
+
+        });
+
+
+    })
+    res.writeHead(302, { 'Location': '/thanks' });
+    res.end();
+}
+
+
+fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+        res.statusCode = 404;
+        res.write("Archivo no exiete");
+        res.end();
+    }
+    else {
+        res.write(data.toString());
+        res.end();
+    } 
+});
+
+});
+
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:3000');
+});
+
+
