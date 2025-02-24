074a1fc175bca544e592b81ad9dce8f894031cb9
youssefDevp <youssefmed212@gmail.com>
2025-02-24 20:33:39 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function validarForm() {
-    
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if (name === "" || email === "") {
+        alert("Name and Email must be filled out");
+        return false;
+    }
+
+    if (!email.includes('@')) {
+        alert("Invalid email address");
+        return false;
+    }
+
+    return true;
 }
- 
